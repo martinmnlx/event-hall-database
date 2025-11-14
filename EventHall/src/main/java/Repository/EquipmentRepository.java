7160ce1bf8faf62b22edf5f0597e553856a63c83
Sam-dot2 <samuel_bongco@dlsu.edu.ph>
2025-11-14 23:17:17 +0800

    Add files via upload

--- EventHall/src/main/java/Repository/EquipmentRepository.java
+++ EventHall/src/main/java/Repository/EquipmentRepository.java
@@ -1,10 +1,23 @@
 package Repository;
 
-// Imports the Entity for Equipment
+// imports the Entity for Equipment
 import Model.Equipment;
-// Imports the tool needed to access the database
+// imports the tool needed to access the database
 import org.springframework.data.jpa.repository. JpaRepository;
+// to be able to write our custom SQL
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface EquipmentRepository extends JpaRepository<Equipment, Integer> {
+
+    // need so we can calculate the total quantity needed to be reserved for equipment_allocation
+    @Query(value = "SELECT SUM(ea.quantity) FROM equipment_allocations ea " +
+            "JOIN reservations r ON ea.reservation_id = r.reservation_id " +
+            "WHERE ea.equipment_id = :equipmentId " +
+            "AND r.status IN ('Confirmed', 'Pending')",
+            nativeQuery = true)
+    Integer getTotalReservedQuantityForEquipmentId(@Param("equipmentId") Integer equipmentId);
 
 }
