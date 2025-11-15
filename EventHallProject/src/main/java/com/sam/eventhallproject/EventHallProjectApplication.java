8d68bc7160094c6755a77e113ecf88af8e705c94
Sam-dot2 <samuel_bongco@dlsu.edu.ph>
2025-11-15 17:15:19 +0800

    Add files via upload

---
+++ EventHallProject/src/main/java/com/sam/eventhallproject/EventHallProjectApplication.java
@@ -0,0 +1,13 @@
+package com.sam.eventhallproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventHallProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventHallProjectApplication.class, args);
+    }
+
+}
---
+++ EventHallProject/src/test/java/com/sam/eventhallproject/EventHallProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sam.eventhallproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventHallProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
